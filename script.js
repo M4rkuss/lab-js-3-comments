faa06a1e1eb0a330f58f99aec59a7692da0b7471
m4rkuss <markbu07@gmail.com>
2022-05-25 17:52:47 +0300

    [+] added main page

---
+++ script.js
@@ -0,0 +1,22 @@
+class Comment {
+  constructor (nam, dat, comm) {
+    this.name = nam;
+    this.date = dat;
+    this.comment = comm;
+  }
+}
+
+let comments = []
+
+document.getElementById('btnOk').addEventListener('click', function () {
+  let date = new Date();
+  document.getElementById("name").innerText = document.getElementById("inputName").value;
+  document.getElementById("date").innerText = date.toLocaleTimeString() + " - " + date.toLocaleDateString();
+  document.getElementById("comment").innerText = document.getElementById('inputText').value;
+  let commentObj = new Comment(document.getElementById("name").innerText, document.getElementById("date").innerText, document.getElementById("comment").innerText);
+  comments.push(commentObj);
+})
+
+document.getElementById('BtnShowAll').addEventListener('click', function () {
+  window.open('all-comments.html', '_blank')
+})
